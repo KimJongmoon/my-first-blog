d0a72d699443c4999967730ce858ec311e842081
종문 <lamon229@gmail.com>
2017-04-06 17:51:02 +0900

    Connecting with Mysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'mysite',
+        'USER': 'ubuntu',
+        'PASSWORD': 'kang2002',
+        'HOST': 'localhost',
     }
 }
 
