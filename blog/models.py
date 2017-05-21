3a857c0e582f491d2096dba80b3aea436521cec0
종문 <lamon229@gmail.com>
2017-05-21 22:43:29 +0900

    pystagram

--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,9 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
-    image = models.ImageField(null = True)
-    filtered_image = models.ImageField(null = True)
-    text = models.TextField()
+    image = models.ImageField(null = True, upload_to = 'photos/%Y/%m/%d/origin')
+    filtered_image = models.ImageField(null = True, upload_to = 'photos/%Y/%m/%d/filtered')
+    text = models.TextField(null = True, blank = True)
     published_date = models.DateTimeField(auto_now_add = True, null = True)
 
     def publish(self):
