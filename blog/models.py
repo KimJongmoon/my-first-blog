2679c0453e3ee88626ae4d8b750610740ad58179
종문 <lamon229@gmail.com>
2017-05-27 22:25:12 +0900

    pystagram

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     image = models.ImageField(null = True, upload_to = '%Y/%m/%d/origin')
-    filtered_image = models.ImageField(null = True, upload_to = '%Y/%m/%d/filtered')
+    #filtered_image = models.ImageField(null = True, upload_to = '%Y/%m/%d/filtered')
     text = models.TextField(null = True, blank = True)
     published_date = models.DateTimeField(auto_now_add = True, null = True)
 
@@ -15,7 +15,7 @@ class Post(models.Model):
 
     def delete(self, *args, **kwargs):
         self.image.delete()
-        self.filtered_image.delete()
+        #self.filtered_image.delete()
         super(Post, self).delete(*args, **kwargs)
 
     def get_absolute_url(self):
